0fe35943f391a39912c68e9b1befce9db1ddb192
usuario <usuario@10.22.143.9>
2014-11-10 14:09:45 +0000

    Generic method: Histogram build

--- src/kata4/HistogramBuilder.java
+++ src/kata4/HistogramBuilder.java
@@ -11,7 +11,8 @@ public class HistogramBuilder<E> {
     public <A> Histogram<A> build(AttributeExtractor<E,A> extractor){
         Histogram histogram = new Histogram<>();
         for (E item : items) {
-            histogram.put(item, histogram.get(item) + 1);
+            A attribute = extractor.extract(item);
+            histogram.put(attribute, histogram.get(attribute) + 1);
         }
         return histogram;
     }   
